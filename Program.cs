488f0ad9de90cd8a31d4e2b9322c96da2b091736
Denis Donskoy <denisdonskoi@gmail.com>
2022-11-28 00:13:18 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter numbers: ");
+int numberFirst = Convert.ToInt32(Console.ReadLine());
+int numberSecond = Convert.ToInt32(Console.ReadLine());
+if (numberFirst < numberSecond)
+ {
+    Console.WriteLine($"Number {numberSecond} is bigger than {numberFirst}");
+ }
+else
+ {
+   Console.WriteLine($"Number {numberFirst} is bigger than {numberSecond}");
+ }
