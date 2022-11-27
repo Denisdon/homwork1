b8a8abd297508be15177d0ba7959ed988d950ebd
Denis Donskoy <denisdonskoi@gmail.com>
2022-11-28 00:40:06 +0300

    vtoraya zadacha

---
+++ Program2.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("enter three numbers: ");
+int numberFirst = Convert.ToInt32(Console.ReadLine());
+int numberSecond = Convert.ToInt32(Console.ReadLine());
+int numberThird = Convert.ToInt32(Console.ReadLine());
+
+if (numberFirst>numberSecond)
+ {
+    if (numberFirst>numberThird)
+    {
+        Console.WriteLine($"Number {numberFirst} is the biggest");
+    } 
+    else 
+    {
+        Console.WriteLine($"Number {numberThird} is the biggest");
+    }
+ }
+ else 
+ {
+    if (numberSecond>numberThird)
+    {
+        Console.WriteLine($"Number {numberSecond} is the biggest");
+    } 
+    else 
+    {
+        Console.WriteLine($"Number {numberThird} is the biggest");
+    }
+    
+ }
